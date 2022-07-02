a5654c2d31d00de13fb1f65d9d3e60746b99a00e
Yusuke Yamasaki <yuu5364yuu@gmail.com>
2022-07-02 16:33:36 +0900

    rosbagのテストをいたしましたわ。良好ですわよ

--- src/rosbag_linetrace.cpp
+++ src/rosbag_linetrace.cpp
@@ -5,17 +5,22 @@
 
 #include "ros/ros.h"
 #include "intelligent_robot_b_2022/rosbagdata.h"
+#include "std_msgs/String.h"
 
 void CallBack(const intelligent_robot_b_2022::rosbagdata &data)
 {
-    ROS_INFO("Sub_Data: L_encoder[%ld] R_encoder[%ld] L_x[%f] R_x[%f] L_v[%f] R_v[%f] v[%f]", data.L_encoder
-                                                                                            , data.R_encoder
-                                                                                            , data.L_x
-                                                                                            , data.R_x
-                                                                                            , data.L_v
-                                                                                            , data.R_v
-                                                                                            , data.v);
+    ROS_INFO("Sub_Data: L_encoder[%d] R_encoder[%d] L_x[%f] R_x[%f] L_v[%f] R_v[%f] v[%f]", data.L_encoder
+                                                                                          , data.R_encoder
+                                                                                          , data.L_x
+                                                                                          , data.R_x
+                                                                                          , data.L_v
+                                                                                          , data.R_v
+                                                                                          , data.v);
 }
+// void Callback(const std_msgs::String &data)
+// {
+//     ROS_INFO("%d",data.data);
+// }
 
 int main(int argc, char **argv)
 {
@@ -23,11 +28,9 @@ int main(int argc, char **argv)
 
     ros::NodeHandle n;
 
-    ros::Subscriber sub = n.subscribe("linetrace_data", 10, CallBack);
+    ros::Subscriber sub = n.subscribe("chatter", 10, CallBack);
 
-    ros::Rate loop_rate(115200); // 単位：Hz_ここの値が、arduinoとの同期周期の定義になる。
-
-    ROS_INFO("NODE: rosbag_linetrace standby.");
+    ros::Rate loop_Rate(16000000);
 
     ros::spin();
 
